6a8badd4e55664030d0f05596b762f49e12ce52c
zienabalawneh <yahyazainab204@gmail.com>
2021-05-17 11:36:37 +0300

    finishead

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+
+
+    render(){
+
+        return (
+        <>
+        <h3> Author: Zienab Alawneh </h3>
+        </>
+        
+        );
+           }
+
+
+     }
+
+
+     export default Footer;
