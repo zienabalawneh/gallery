56fbb0bc90680f272df60528525d418b03220683
zienabalawneh <yahyazainab204@gmail.com>
2021-05-19 12:58:22 +0300

    lab3

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,52 +1,29 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal'
-
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 
 class SelectedBeast extends React.Component {
-
-
-    final = () => {
-
-        this.props.changeInfo();
-    }
 
     render() {
 
         return (
             <>
-
-
-           
-{/* <Modal show={true} onHide={true} animation={false}>
-<Modal.Header closeButton>
-   <Modal.Title> {this.props.title}</Modal.Title>
- </Modal.Header>
- <Modal.Body>
-    
- <Modal.Img src={this.props.imgUrl} alt={this.props.title}  />   
- {this.props.description}
-     </Modal.Body>
- <Modal.Footer>
-   <Button variant="secondary" onClick={true}>
-     Close
-  </Button>
- 
- </Modal.Footer>
-</Modal> */}
-
+                   <Modal show={this.props.show} onHide={this.props.handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>  {this.props.myarr.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body> {this.props.myarr.description}  </Modal.Body>
+                    <Card.Img style={{ width: '300px' }} variant="top" src={this.props.myarr.image_url} />
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={this.props.handleClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
             </>
-
-        );
+        )
     }
-
-
-
-
-
 }
 export default SelectedBeast;
-
 
